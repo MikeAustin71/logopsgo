24560da3a621a9bed286f7a706c8330e5d05b133
Mike Rapp <mike@paladinacs.net>
2017-05-19 22:10:35 -0500

    Added Test: TestLogJobGroupConfig_New

---
+++ common/logjobgroupconfig_test.go
@@ -0,0 +1,57 @@
+package common
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestLogJobGroupConfig_New(t *testing.T) {
+	parms := LogStartupParameters{}
+	thisSrcFileName := "logjobgroupconfig_test.go"
+	thisMethodName := "TestLogJobGroupConfig_New"
+	thisErrBlockNo := int64(80000)
+	parms.StartTime = time.Now()
+	parms.AppVersion = "2.0.0"
+	parms.LogMode = LogVERBOSE
+	parms.AppLogDir = "./CmdrX"
+	parms.AppName = "CmdrX"
+	parms.AppExeFileName = "CmdrX.exe"
+	parms.NoOfJobs = 37
+	parms.CommandFileName = "CmdrX.xml"
+
+	parent := ErrBaseInfo{}.GetNewParentInfo(thisSrcFileName, thisMethodName, thisErrBlockNo)
+
+	lg := LogJobGroupConfig{}
+
+	se := lg.New(parms, parent)
+
+	if se.IsErr {
+		t.Error("Expected se.IsErr == false, got", se.IsErr)
+	}
+
+	if parms.CommandFileName != lg.CommandFileName {
+		t.Error(fmt.Sprintf("Expected CommandFileName = %v, got ", parms.CommandFileName), lg.CommandFileName)
+	}
+
+	if parms.StartTime != lg.StartTime {
+		t.Error(fmt.Sprintf("Expected Start Time = %v, got", parms.StartTime.String()), lg.StartTime.String())
+	}
+
+	if parms.AppVersion != lg.AppVersion {
+		t.Error(fmt.Sprintf("Expected App Version = %v, got", parms.AppVersion), lg.AppVersion)
+	}
+
+	if parms.AppName != lg.AppName {
+		t.Error(fmt.Sprintf("Expected App Name = %v, got", parms.AppName), lg.AppName)
+	}
+
+	if parms.LogMode != lg.LogMode {
+		t.Error(fmt.Sprintf("Expected Log Mode = %v, got", parms.LogMode), lg.LogMode)
+	}
+
+	if parms.NoOfJobs != lg.AppNoOfJobs {
+		t.Error(fmt.Sprintf("Expected Number Of Jobs = %v, got", parms.NoOfJobs), lg.AppNoOfJobs)
+	}
+
+}
