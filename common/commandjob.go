9efaf28ad3064c25954050e704707b85bf774694
Mike Rapp <mike@paladinacs.net>
2017-05-19 22:53:57 -0500

    Added CommandJob structure

---
+++ common/commandjob.go
@@ -0,0 +1,34 @@
+package common
+
+import "time"
+
+// CommandJobGroup - Contains a
+// slice of CommandJobInfo structures
+// plus Group Header Info.
+type CommandJobGroup struct {
+	CmdFileVersion          string
+	DefaultCmdExeDir        string
+	LogPathFileName					string
+	LogFileRetention        time.Duration
+	KillAllJobsOnFirstError bool
+}
+
+// CommandJobInfo - Contains information on
+// a Command Job to be executed
+type CommandJobInfo struct {
+	DisplayName                   string
+	Description                   string
+	CommandType                   string
+	ExeDir                        string
+	DelayCmdStart                 time.Duration
+	StartCmdDateTime              time.Time
+	KillJobsOnExitCodeGreaterThan int
+	KillJobsOnExitCodeLessThan    int
+	CommandTimeOut                time.Duration
+	ExeCommand                    string
+	CmdExeStartTime               time.Time
+	CmdExeEndTime                 time.Time
+	CmdExeElapsedTime             time.Duration
+	KillAllJobsOnFirstError       bool
+	JobExitCode                   int
+}
